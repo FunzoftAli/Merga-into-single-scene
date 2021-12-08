b2d8ba760e2e01005a5d78cc6c7e2461bf5f8993
FunzoftAli <funzoftali@gmail.com>
2021-12-08 10:50:23 +0500

    Revert "Revert "Add Canvas Manager""
    This reverts commit 744eb4616c80d6813678f54cc4f3e4782277745b.

---
+++ SingleSceneWork/Assets/Scripts/CanvasManager.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanvasManager : MonoBehaviour
+{
+    public GameObject txtPeople;
+    public GameObject btnPeople;
+}
